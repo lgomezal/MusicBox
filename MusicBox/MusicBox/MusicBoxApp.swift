598340059e5f74fe5f321434147008ae033a4d2b
Luis Gomez Alonso <lgomezal@gmail.com>
2021-11-16 20:33:00 +0100

    Implement login

--- MusicBox/MusicBox/MusicBoxApp.swift
+++ MusicBox/MusicBox/MusicBoxApp.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 import Combine
 import SpotifyWebAPI
+import KeychainAccess
 
 @main
 struct MusicBoxApp: App {
+    
+    //To delete keychain execute
+    //let keychain = Keychain(service: "com.codigohifi.MusicBox")
+    //let authorizationManagerKey = "authorizationManager"
     
     @StateObject var spotify = Spotify()
 
     init() {
         SpotifyAPILogHandler.bootstrap()
+        //To delete keychain execute
+        /*do {
+            /*
+             Remove the authorization information from the keychain.
+
+             If you don't do this, then the authorization information that you
+             just removed from memory by calling
+             `SpotifyAPI.authorizationManager.deauthorize()` will be retrieved
+             again from persistent storage after this app is quit and
+             relaunched.
+             */
+            try keychain.remove(self.authorizationManagerKey)
+            print("did remove authorization manager from keychain")
+            
+        } catch {
+            print(
+                "couldn't remove authorization manager " +
+                "from keychain: \(error)"
+            )
+        }*/
     }
 
     var body: some Scene {
--- MusicBox/MusicBox/Views/RootView.swift
+++ MusicBox/MusicBox/Views/RootView.swift
@@ -28,7 +28,7 @@ struct RootView: View {
         // the login process successfully completes, `Spotify.isAuthorized` will
         // be set to `true` and `LoginView` will be dismissed, allowing the user
         // to interact with the rest of the app.
-        //.modifier(LoginView())
+        .modifier(LoginView())
         // Presented if an error occurs during the process of authorizing with
         // the user's Spotify account.
         .alert(item: $alert) { alert in
@@ -40,6 +40,74 @@ struct RootView: View {
     }
     
     func handleURL(_ url: URL) {
+        // **Always** validate URLs; they offer a potential attack vector into
+        // your app.
+        guard url.scheme == self.spotify.loginCallbackURL.scheme else {
+            print("not handling URL: unexpected scheme: '\(url)'")
+            self.alert = AlertItem(
+                title: "Cannot Handle Redirect",
+                message: "Unexpected URL"
+            )
+            return
+        }
+        
+        print("received redirect from Spotify: '\(url)'")
+        
+        // This property is used to display an activity indicator in `LoginView`
+        // indicating that the access and refresh tokens are being retrieved.
+        spotify.isRetrievingTokens = true
+        
+        // Complete the authorization process by requesting the access and
+        // refresh tokens.
+        spotify.api.authorizationManager.requestAccessAndRefreshTokens(
+            redirectURIWithQuery: url,
+            // This value must be the same as the one used to create the
+            // authorization URL. Otherwise, an error will be thrown.
+            state: spotify.authorizationState
+        )
+        .receive(on: RunLoop.main)
+        .sink(receiveCompletion: { completion in
+            // Whether the request succeeded or not, we need to remove the
+            // activity indicator.
+            self.spotify.isRetrievingTokens = false
+            
+            /*
+             After the access and refresh tokens are retrieved,
+             `SpotifyAPI.authorizationManagerDidChange` will emit a signal,
+             causing `Spotify.authorizationManagerDidChange()` to be called,
+             which will dismiss the loginView if the app was successfully
+             authorized by setting the @Published `Spotify.isAuthorized`
+             property to `true`.
+
+             The only thing we need to do here is handle the error and show it
+             to the user if one was received.
+             */
+            if case .failure(let error) = completion {
+                print("couldn't retrieve access and refresh tokens:\n\(error)")
+                let alertTitle: String
+                let alertMessage: String
+                if let authError = error as? SpotifyAuthorizationError,
+                   authError.accessWasDenied {
+                    alertTitle = "You Denied The Authorization Request :("
+                    alertMessage = ""
+                }
+                else {
+                    alertTitle =
+                        "Couldn't Authorization With Your Account"
+                    alertMessage = error.localizedDescription
+                }
+                self.alert = AlertItem(
+                    title: alertTitle, message: alertMessage
+                )
+            }
+        })
+        .store(in: &cancellables)
+        
+        // MARK: IMPORTANT: generate a new value for the state parameter after
+        // MARK: each authorization request. This ensures an incoming redirect
+        // MARK: from Spotify was the result of a request made by this app, and
+        // MARK: and not an attacker.
+        self.spotify.authorizationState = String.randomURLSafe(length: 128)
         
     }
 }
